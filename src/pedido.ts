3f87ccc340536459503b05ef5420fdf606664c47
Barrera <barrerasara2008@gmail.com>
2025-11-02 16:49:21 -0500

    Segundo commit

--- src/pedido.ts
+++ src/pedido.ts
@@ -1,11 +1,11 @@
-function tomarPedido(){
-    return new promise((resolve) => {
+function tomarPedido() {
+    return new Promise((resolve) => {
         setTimeout(()=> {
             resolve("Pedido recibido");
         }, 1000);
     })
 }
-functionprepararPedido(){
+function prepararPedido(){
     return new Promise ((resolve, reject) => {
         setTimeout (() => {
             if (Math.random() < 0.3){
@@ -16,3 +16,22 @@ functionprepararPedido(){
         }, 2000)
     })
 }
+ function entregarPedido (){
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve ("Pedido entregado al cliente")
+        }, 1500)
+    })
+ }
+
+ tomarPedido()
+ .then ((msg) => {
+    console.log(msg);
+    return prepararPedido();
+ })
+ .then ((msg) => {
+    console.log (msg);
+    return entregarPedido();
+ })
+ .then ((msg) => console.log(msg))
+ .catch ((err) => console.log (err));
---
+++ src/usuario.ts
@@ -0,0 +1,23 @@
+function getUsers(){
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+            resolve(["User1","User2"]);
+        }, 2000)
+    });
+}
+
+function getPosts(){
+    return new Promise ((resolve) => {
+        setTimeout (() => {
+            resolve(["Post1","Post2"]);
+        },3000)
+    })
+}
+
+Promise.all([getUsers(), getPosts()])
+.then((resultados) => {
+
+})
+.catch(() =>{
+    console.log("Error al cargar los datos")
+})
